20b1404a9596fd0faadd078694735b6d7d3687bd
Mrinal Chaubey <mrinalchaubey7@gmail.com>
2025-12-10 13:27:20 +0530

    Intial Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-green-500 font-bold">
+  Tailwind Working!
+</h1>
+    </div>
+  )
+}
+
+export default App
