0ba16157ac0fa61e7b91ee2018544ae574012693
Mrinal Chaubey <mrinalchaubey7@gmail.com>
2025-12-14 20:30:53 +0530

    Get User Api Route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from "express" 
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+
+
 
 dotenv.config()
 
@@ -18,6 +22,7 @@ mongoose
 
 const app = express()
 
+app.use(cookieParser()) 
 
 // for allowing json object in req body
 app.use(express.json())
@@ -28,7 +33,7 @@ app.listen(3000, () => {
 })
 
 app.use("/api/auth", authRoutes)
-
+app.use("/api/user", userRoutes)
 
 
 
